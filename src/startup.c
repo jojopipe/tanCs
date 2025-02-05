2061c9e6c46ace254eceb8f3ea9b72c202df7d5e
jojopipe <78083515+jojopipe@users.noreply.github.com>
2025-02-06 00:29:24 +0100

    initial commit

--- src/startup.c
+++ src/startup.c
@@ -11,13 +11,13 @@ int init_game() {
     struct window_parameters wparam;
     wparam.width = 1200;
     wparam.height = 700;
-    wparam.window_state = 1;
+    wparam.window_state = 0;
 
     InitWindow(wparam.width, wparam.height, "tanCs");
     if (wparam.window_state == 1) ToggleBorderlessWindowed();
     SetTargetFPS(144);
     Camera camera = { 0 };
-    camera.position = (Vector3) {0.0f, 25.0f, 25.0f};
+    camera.position = (Vector3) {25.0f, 25.0f, 25.0f};
     camera.target = (Vector3) {0.0f, 0.0f, 0.0f};
     camera.up = (Vector3) {0.0f, 1.0f, 0.0f};
     camera.fovy = 70.0f;
@@ -26,16 +26,18 @@ int init_game() {
     Model characterModel = LoadModel("assets/Tanks/tnk_tank_p.glb");
     Vector3 tank_pos = {0.0f, 0.0f, 0.0f};
 
+    Color bg_color = BLUE;
+
     while(!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+        ClearBackground(bg_color);
         BeginMode3D(camera);
 
-        DrawMesh(characterModel.meshes[0], characterModel.materials[1], characterModel.transform);
-
-        DrawGrid(15, 1.0f);
+        //DrawModel(characterModel, (Vector3){0.0f,1.0f,0.0f}, 1.0f, RED);
+        DrawGrid(15, 3.0f);
 
         EndMode3D();
+        DrawFPS(10,10);
         EndDrawing();
     }
 
